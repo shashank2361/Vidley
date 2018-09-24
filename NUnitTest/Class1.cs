f060cec9841461149cee6909673ded2d0a0f0aef
DESKTOP-SB3UMDG\Shashank-PC <Shashank2361@yahoo.co.in>
2018-09-24 15:11:21 +0100

    Added NUnit , Unity , Generic repository,  Unitof Work It had different controllers using the different patterns

---
+++ NUnitTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vidley.DAL;
+using Vidley.Models;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class CheckDepartmentTest
+    {
+        [Test]
+        public void CheckEmployeeExist()
+
+        {
+            using (var unitOfWork = new UnitOfWork(new ApplicationDbContext()))
+            {
+                var employee = unitOfWork.employeeGenericRepository.GetModelById(5);
+                Assert.IsNotNull(employee.Id, "not found");
+            }
+
+
+        }
+    }
+}
